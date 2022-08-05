1d608d7e16605ba3a4ff8a27db4c361de81333f8
Mustansir Hussain <icoolguy1995@gmail.com>
2022-08-05 20:32:24 +0200

    initial creation of a new function to take over the existing one Replaced IFs by When

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -1,6 +1,47 @@
 package guru.drako.examples.gildedrose
 
 class Shop(val items: List<Item>) {
+
+  fun updateInventory(){
+    for (item in items) {
+      when(item.name){
+        "Backstage passes to a TAFKAL80ETC concert"->{
+          if(item.sellIn >0 && item.quality <50){
+            if(item.sellIn <6 ){
+              item.quality+=3
+            } else if (item.sellIn <11){
+              item.quality+=2
+            }else{
+              item.quality++
+            }
+          }else if(item.sellIn <0){
+            item.quality = 0
+          }
+        }
+        "Aged Brie"->{
+          if(item.quality<50)
+            item.quality++
+        }
+        "Sulfuras, Hand of Ragnaros"->{
+          //do nothing for the iteration ;)
+        }
+        "Conjured Mana Cake"->{
+          if(item.quality>1){
+            item.quality -=2
+          }else if(item.quality ==1){
+            item.quality = 0
+          }
+        }
+        else->{
+          if(item.quality >0){
+            item.quality--
+          }
+        }
+
+      }
+    }
+  }
+
   fun updateQuality() {
     for (item in items) {
       if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
